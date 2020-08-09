f5902d3ce2851b13b4e3dac764e8e8888af13cad
Corentin Boiteau <Corenb@users.noreply.github.com>
2020-08-09 23:25:37 +0200

    Adjust environment variables

--- start.sh
+++ start.sh
@@ -36,6 +36,21 @@ else
   echo "Not time to reset end dimension."
 fi
 
+if (($1 < 12000))
+then
+   G1NewSizePercent=30
+   G1MaxNewSizePercent=40
+   G1HeapRegionSize=8
+   G1ReservePercent=20
+   InitiatingHeapOccupancyPercent=15
+else
+   G1NewSizePercent=40
+   G1MaxNewSizePercent=50
+   G1HeapRegionSize=16
+   G1ReservePercent=15
+   InitiatingHeapOccupancyPercent=20
+fi
+
 # Start server
 echo "Starting server..."
-java -Xms$1M -Xmx$1M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=20 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -Dterminal.jline=false -Dterminal.ansi=true -Dfile.encoding=UTF-8 -Dcom.mojang.eula.agree=true -jar server.jar nogui
+java -Xms$1M -Xmx$1M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=$G1NewSizePercent -XX:G1MaxNewSizePercent=$G1MaxNewSizePercent -XX:G1HeapRegionSize=$G1HeapRegionSizeM -XX:G1ReservePercent=$G1ReservePercent -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=$InitiatingHeapOccupancyPercent -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -Dterminal.jline=false -Dterminal.ansi=true -Dfile.encoding=UTF-8 -Dcom.mojang.eula.agree=true -jar server.jar nogui
